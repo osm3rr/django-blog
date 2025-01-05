91433950b2de4fe42410046c0e25fcef4fdd5b9f
osm3rr <osm3rr@adakademy.com>
2025-01-05 18:52:02 -0400

    publication create done

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView # new
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView # new
 from .models import Publication
 
 # Create your views here.
@@ -6,6 +7,11 @@ class PublicationListView(ListView):
     model = Publication
     template_name = "publications-list.html"
 
-class PublicationDetailView(DetailView): # new
+class PublicationDetailView(DetailView):
     model = Publication
     template_name = "publication-detail.html"
+
+class PublicationCreateView(CreateView): # new
+    model = Publication
+    template_name = "publication-create.html"
+    fields = ['title', 'body', 'author']
