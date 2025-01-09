043215fd5cd6dfff91aad5c5a88656b610d820a3
osm3rr <osm3rr@adakademy.com>
2025-01-09 16:02:09 -0400

    publication delete done

--- blog/urls.py
+++ blog/urls.py
@@ -3,12 +3,14 @@ from .views import (
                     PublicationListView,
                     PublicationDetailView,
                     PublicationCreateView, 
-                    PublicationUpdateView, # new
+                    PublicationUpdateView,
+                    PublicationDeleteView, # new
                     ) 
 
 urlpatterns = [
     path("", PublicationListView.as_view(), name="publications-list"),
     path("publication/<int:pk>/", PublicationDetailView.as_view(), name="publication-detail"),
     path("publication/create/", PublicationCreateView.as_view(), name="publication-create"), 
-    path("publication/<int:pk>/update/", PublicationUpdateView.as_view(), name="publication-update"), # new
+    path("publication/<int:pk>/update/", PublicationUpdateView.as_view(), name="publication-update"),
+    path("publication/<int:pk>/delete/", PublicationDeleteView.as_view(), name="publication-delete"), # new
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView # new
+from django.views.generic.edit import (
+                                        CreateView,
+                                        UpdateView,
+                                        DeleteView, # new
+                                        ) 
 from .models import Publication
+from django.urls import reverse_lazy # new
 
 # Create your views here.
 class PublicationListView(ListView):
@@ -16,7 +21,12 @@ class PublicationCreateView(CreateView):
     template_name = "publication-create.html"
     fields = ['title', 'body', 'author']
 
-class PublicationUpdateView(UpdateView): # new
+class PublicationUpdateView(UpdateView):
     model = Publication
     template_name = "publication-update.html"
     fields = ['title', 'body']
+
+class PublicationDeleteView(DeleteView): # new
+    model = Publication
+    template_name = "publication-delete.html"
+    success_url = reverse_lazy("publications-list")
